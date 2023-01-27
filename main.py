bd6f6801046120f4da7fba8edadf79446258c058
Vincent Donkor <dvincent@dvincent9CH2R.vmware.com>
2023-01-27 09:53:26 -0500

    default to reading input from file

--- main.py
+++ main.py
@@ -8,9 +8,9 @@ import json
 async def main():
     num_proc = asyncio.Semaphore(value=8)
 
-    # downloads = utils.read_json_files()
-    # download_requests = [utils.ComponentDownload(**payload) for payload in downloads]
-    download_requests = [utils.ComponentDownload(**payload) for payload in DOWNLOAD_REQUEST]
+    downloads = utils.read_json_files()
+    download_requests = [utils.ComponentDownload(**payload) for payload in downloads]
+    # download_requests = [utils.ComponentDownload(**payload) for payload in DOWNLOAD_REQUEST]
 
     tasks = [asyncio.create_task(utils.download_file(download, num_proc)) for download in download_requests]
 
