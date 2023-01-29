3abb45c58ee6e0b4a81f0157210b973caa23eac8
Vincent Donkor <dvincent@dvincent9CH2R.vmware.com>
2023-01-28 22:53:52 -0500

    adding logging

--- log.py
+++ log.py
@@ -2,6 +2,7 @@ import logging
 import os
 from pathlib import Path
 from dotenv import load_dotenv
+
 load_dotenv()
 
 # Create a logger object
@@ -9,6 +10,7 @@ logger = logging.getLogger(__name__)
 logger.setLevel(logging.DEBUG)
 
 log_file = Path(f"{os.getenv('BASE_DIR')}/logs/download.log")
+log_file.mkdir(parents=True, mode=0o775, exist_ok=True)
 file_handler = logging.FileHandler(filename=str(log_file))
 
 file_handler.setLevel(logging.DEBUG)
@@ -18,5 +20,3 @@ file_fmt = logging.Formatter('%(asctime)s - %(levelname)s - %(message)s')
 file_handler.setFormatter(file_fmt)
 
 logger.addHandler(file_handler)
-
-
